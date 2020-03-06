43729472997fad23a63f1d8e1b3c817672d8122e
Андрей Заболотный <zapparello@ya.ru>
2020-03-06 16:35:01 +0300

    Added setjmp() prototypes to setjmp.h. Fixed a typo in time.h.

--- include/nuttx/lib/setjmp.h
+++ include/nuttx/lib/setjmp.h
@@ -61,4 +61,7 @@
  * Public Function Prototypes
  ****************************************************************************/
 
+int setjmp(jmp_buf env);
+void longjmp(jmp_buf env, int val) noreturn_function;
+
 #endif /* __INCLUDE_NUTTX_LIB_SETJMP_H */
